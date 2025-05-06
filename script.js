86651c508d3810be9129f5eb500e5fc1b8ec8990
youssefali33 <x.marketingg@gmail.com>
2025-05-06 17:14:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+});
